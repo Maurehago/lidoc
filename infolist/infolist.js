13df92020f9843dde042a0dc2ffda0bf1de7fafc
Roman <r.huber@bsf.at>
2024-07-17 17:13:03 +0200

    infolist.js InfoTable eingebaut

--- infolist/infolist.js
+++ infolist/infolist.js
@@ -1,20 +1,21 @@
 // Infoliste
 
 // Global Short Identifier
-export function GSID () {
-    return new Date().getTime().toString(36) + crypto.getRandomValues(new Uint32Array(1))[0].toString(36);
+export function GSID() {
+    return new Date().getTime().toString(36) +
+        crypto.getRandomValues(new Uint32Array(1))[0].toString(36);
 }
 
 export class InfoList {
     // Construktion
     constructor(name) {
-        this.Name = "";         // Name der Liste
-        this.Path = "";         // Pfad / Gruppe
-        this.Prop = new Map();  // Zusätzliche Eigenschaften der Liste
-        this.Fields = [];       // FeldNamen von Data
-        this.Types = [];        // FeldTypen von Data
-        this.Data = new Map();  // Daten der Liste
-        this.List = [];         // Wenn Auflistung statt Daten. z.B.: bei ENums
+        this.Name = ""; // Name der Liste
+        this.Path = ""; // Pfad / Gruppe
+        this.Prop = new Map(); // Zusätzliche Eigenschaften der Liste
+        this.Fields = []; // FeldNamen von Data
+        this.Types = []; // FeldTypen von Data
+        this.Data = new Map(); // Daten der Liste
+        this.List = []; // Wenn Auflistung statt Daten. z.B.: bei ENums
 
         if (name) {
             this.Name = name;
@@ -36,12 +37,11 @@ export class InfoList {
         // Alle Felder durchgehen
         fields.forEach((name, index) => {
             obj[name] = data[index];
-        })
+        });
 
         // Objekt zurückgeben
         return obj;
     } // getAsObject
-
 
     // Setzt einen Datensatz(data) mit der id in der Liste
     // Wenn vorhanden wird der Datensatz komplett überschrieben.
@@ -65,12 +65,12 @@ export class InfoList {
         const indexes = [];
         const fieldList = this.Fields;
 
-        fields.forEach((v,i) => {
+        fields.forEach((v, i) => {
             if (v.indexOf(" ") >= 0) {
                 v = v.split(" ")[0];
-            } 
+            }
             indexes[i] = fieldList.indexOf(v);
-        })
+        });
 
         return indexes;
     }
@@ -104,7 +104,7 @@ export class InfoList {
             } else {
                 data[i] = null;
             }
-        })
+        });
         return data;
     }
 
@@ -122,17 +122,17 @@ export class InfoList {
 
             // Wert setzen
             switch (fieldType) {
-            case "int", "num", "bool":
-                dataList[index] = value;
-                break;
+                case "int", "num", "bool":
+                    dataList[index] = value;
+                    break;
 
-            default:
-                dataList[index] = "" + value;
+                default:
+                    dataList[index] = "" + value;
             }
         }
     } // setValue
 
-    // Sortierter Index 
+    // Sortierter Index
     getSortIndex(fields) {
         // Alle Keys von der map
         const dataIndex = Array.from(this.Data.keys());
@@ -146,16 +146,16 @@ export class InfoList {
             // Aktuellen nicht sortierten Index zurückgeben
             return dataIndex;
         }
-        
+
         // Sortieren
-        dataIndex.sort((a,b) =>{
+        dataIndex.sort((a, b) => {
             const dataA = this.Data.get(a);
             const dataB = this.Data.get(b);
 
             let sort = 0;
 
             // Prüfen
-            fieldIndex.some((d, i) =>{
+            fieldIndex.some((d, i) => {
                 const fieldName = fields[i];
 
                 // Wenn Absteigend
@@ -178,15 +178,14 @@ export class InfoList {
                     }
                 }
                 return false;
-            }) // Prüfen
+            }); // Prüfen
 
             // Sortierung zurückgeben
             return sort;
-        })
+        });
 
         return dataIndex;
     } // getSortIndex
-
 
     // Index Filtern
     // eine Funktion die True oder False zurückliefert mit übergeben
@@ -199,27 +198,26 @@ export class InfoList {
         const fields = this.Fields;
 
         // Alle Daten durchgehen
-        this.Data.forEach((data, id) =>{
+        this.Data.forEach((data, id) => {
             const obj = {};
-    
+
             // Alle Felder durchgehen
             fields.forEach((name, index) => {
                 obj[name] = data[index];
-            })
-    
+            });
+
             if (filterFunc(obj, ...params)) {
                 filterIndex.push(id);
             }
-        })
+        });
 
         return filterIndex;
     } // get FilterIndex
 
-
     // Tabellen Daten lesen
     getTableDataHTML(fields, index) {
         const fieldIndex = this.getFieldIndexes(fields);
-        let tableDataString = ""
+        let tableDataString = "";
 
         // Daten in String
         function dataToString(data, id) {
@@ -234,8 +232,8 @@ export class InfoList {
                 if (typeof content === "string") {
                     content = content.replaceAll("\n", "</br>");
                 }
-                dataString += '<td>' + content + '</td>';
-            })
+                dataString += "<td>" + content + "</td>";
+            });
 
             // Ende Datenzeile
             dataString += "</tr>";
@@ -249,12 +247,12 @@ export class InfoList {
             index.forEach((value) => {
                 const data = this.Data.get(value);
                 tableDataString += dataToString(data, value);
-            })
+            });
         } else {
             // Alle Einträge in der map
             this.Data.forEach((data, key) => {
                 tableDataString += dataToString(data, key);
-            })
+            });
         }
 
         return tableDataString;
@@ -264,11 +262,11 @@ export class InfoList {
     stringify() {
         // in JSON String umwandeln
         const il = {};
-        il.Name = this.Name;         // Name der Liste
-        il.Prop = Object.fromEntries(this.Prop);  // Zusätzliche Eigenschaften der Liste
-        il.Fields = this.Fields;       // FeldNamen von Data
-        il.Types = this.Types;        // FeldTypen von Data
-        il.Data = Object.fromEntries(this.Data);   // Daten der Liste
+        il.Name = this.Name; // Name der Liste
+        il.Prop = Object.fromEntries(this.Prop); // Zusätzliche Eigenschaften der Liste
+        il.Fields = this.Fields; // FeldNamen von Data
+        il.Types = this.Types; // FeldTypen von Data
+        il.Data = Object.fromEntries(this.Data); // Daten der Liste
 
         return JSON.stringify(il);
     } // stringify
@@ -282,7 +280,7 @@ export class InfoList {
         // String in Javascript Objekt
         const obj = JSON.parse(listString);
 
-        // Infoliste 
+        // Infoliste
         Object.assign(this, obj);
 
         // nur infolist Prop und Data
@@ -324,7 +322,7 @@ export class InfoList {
             // Json lesen
             const obj = await response.json();
 
-            // Infoliste 
+            // Infoliste
             Object.assign(this, obj);
 
             // nur infolist Prop und Data
@@ -371,8 +369,8 @@ export class InfoList {
 
         try {
             const response = await fetch(url, {
-                method: "POST"
-                , body: this.stringify()
+                method: "POST",
+                body: this.stringify(),
             });
             if (!response.ok) {
                 throw new Error("Response Status: " + response.status);
@@ -385,66 +383,162 @@ export class InfoList {
     } // send
 } // Class InfoList
 
-// Map von Infolisten
-export class ILists extends Map {
-    constructor() {
-        super();
+//  Info Tabelle
+export class InfoTable {
+    constructor(elm, infolist, cols, colTitles) {
+        // Eigenschaften
+        this.elm = elm;
+        this.infoList = infolist;
+        this.cols = cols;
+        this.colTitles = colTitles || [];
+        this.sortIndex = [];
+
+        // todo: Gruppierung
+        // todo: Filter
+        // todo: Formular
+
+        // ----- Event Funktionen ------
+        this.#checkKeys.bind(this);
     }
 
-    // liefert einen JSON-String von der InfoList Auflistung zurück.
-    // Für Datenübertragung und zum Speichern in eine Datei.
-    Marshal() {
-        // in JSON String umwandeln
-        const obj = {};
+    //  Setzt das HTML-Element für die Tabelle
+    setElement(elm) {
+        this.elm = elm;
+    };
 
-        // Alle Listen durchgehen
-        this.forEach((value, key) => {
-            // Value ist InfoList
-            if (value instanceof InfoList) {
-                const il = {};
-                il.Name = value.Name;         // Name der Liste
-                il.Prop = Object.fromEntries(value.Prop);  // Zusätzliche Eigenschaften der Liste
-                il.Fields = value.Fields;       // FeldNamen von Data
-                il.Types = value.Types;        // FeldTypen von Data
-                il.Data = Object.fromEntries(value.Data);   // Daten der Liste
-                obj[key] = il;
-            }
-        }) // forEach Infolist
+    //  Setzt die InfoListe für die Tabelle
+    setInfoList(infolist) {
+        this.infoList = infolist;
+    };
 
-        return JSON.stringify(obj);
-    } // Marshal
+    // Setzt die Spalten
+    setCols(cols, colTitles) {
+        this.cols = cols;
+        if (colTitles) {
+            this.colTitles = colTitles;
+        }
+    };
 
-    // Liest einen JsonBlob([]byte) in die InfoList Auflistung ein
-    Unmarshal(listString, removeAll) {
-        // todo: data(JSONstring) in Objekte umwandeln
-        if (typeof listString != "string") {
+    // Sortierung
+    setSortIndex(sortIndex) {
+        this.sortIndex = sortIndex;
+    }
+
+    // todo: Gruppierungen
+
+    // todo: filter
+
+    // todo: Formular
+
+        // Tabellen Daten lesen
+    #getDataHTML() {
+        let tableDataString = "";
+        const fieldIndex = this.infoList.getFieldIndexes(this.cols);
+
+        // console.log("#getDataHTML");
+
+        // Daten in String
+        function dataToString(data, id) {
+            let dataString = "";
+
+            // Datenzeile
+            dataString = '<tr id="' + id + '">';
+
+            // Feldindex durchgehen
+            fieldIndex.forEach((index) => {
+                let content = data[index];
+                if (typeof content === "string") {
+                    content = content.replaceAll("\n", "</br>");
+                }
+                dataString += "<td>" + content + "</td>";
+            });
+
+            // Ende Datenzeile
+            dataString += "</tr>";
+
+            return dataString;
+        } // dataToString
+
+        // wenn Index
+        if (this.sortIndex && this.sortIndex.length > 0) {
+            // Alle Daten nach Index durchgehen
+            this.sortIndex.forEach((value) => {
+                const data = this.infoList.Data.get(value);
+                tableDataString += dataToString(data, value);
+            });
+        } else {
+            // Alle Einträge in der map
+            this.infoList.Data.forEach((data, key) => {
+                tableDataString += dataToString(data, key);
+            });
+        }
+
+        // console.log("tableDataString");
+
+        return tableDataString;
+    }; // getTableDataHTML
+
+    // Daten Anzeigen
+    showData() {
+        // Prüfen
+        if (!this.elm instanceof HTMLTableElement) {
+            return;
+        }
+        if (!this.infoList instanceof InfoList) {
+            return;
+        }
+        if (!Array.isArray(this.cols)) {
             return;
         }
 
-        if (removeAll) {
-            this.clear()
+        // console.log("showData:");
+
+        // Body Element
+        const tbodyElm = this.elm.querySelector("tbody");
+        if (!tbodyElm) {
+            tbodyElm = document.createElement("tbody");
+            this.elm.appendChild(tbodyElm);
         }
 
-        // String in Javascript Objekt
-        const obj = JSON.parse(listString);
+        // console.log("tbodyElm:", tbodyElm);
 
-        // Alle Listen durchgehen
-        Object.entries(obj).forEach(([key, value]) => {
-            // Infoliste 
-            const il = new InfoList(key);
-            Object.assign(il, value);
+        // HTML Daten von Liste erzeugen und anzeigen
+        const innerHTML = this.#getDataHTML();
+        // console.log("innerHTML:", innerHTML);
+        tbodyElm.insertAdjacentHTML("beforeend", innerHTML);        
+    } // schowData
 
-            // nur infolist Prop und Data
-            //let myObject = new Map(Object.entries(raw_data));
-            il.Prop = new Map(Object.entries(il.Prop));
-            il.Data = new Map(Object.entries(il.Data));
+    // ====================
+    //   Events
+    // ---------
 
-            this.set(key, il);
-        });
+    // Auf Taste prüfen
+    #checkKeys(e) {
+        console.log("keycode:", e.code);
+        switch (e.code) {
+          case "ArrowUp":
+            // some code here…
+            break;
+          case "ArrowDown":
+            // some code here…
+            break;
+        }
+    }
 
-        return "";
-    } // unmarshal
-} // class IList
+    //   Event Handler
+    // -------------------------
+
+    // Events Registrieren
+    registerEvents() {
+        window.addEventListener("keyup", this.#checkKeys);
+    }
+
+    // Events entfernen
+    unregisterEvents() {
+        window.removeEventListener("keyup", this.#checkKeys);
+    }
+
+} // class InfoTable
 
 
 // Funktion die alle Tabellen mit Daten füllt
@@ -456,16 +550,17 @@ export class ILists extends Map {
 export function checkTables() {
     // Alle Tabellen lesen
     const tables = document.querySelectorAll("table[data-ilist]");
-    
+
     // alle Tabellen durchgehen
     tables.forEach((tableElm) => {
-        const tbodyElm = tableElm.querySelector("tbody");
+        // const tbodyElm = tableElm.querySelector("tbody");
         const sortString = tableElm.getAttribute("data-sort");
-        if (tbodyElm) {
-            tbodyElm.innerHTML = "";
-        } else {
-            tbodyElm = tableElm;
-        }
+        // if (tbodyElm) {
+        //     tbodyElm.innerHTML = "";
+        // } else {
+        //     tbodyElm = tableElm;
+        // }
+
 
         // Infolist Information lesen
         const iListInfo = tableElm.dataset.ilist.split("/"); // iListInfo[0] = Path / iListInfo [1] = Name
@@ -485,7 +580,11 @@ export function checkTables() {
         heads.forEach((headElm) => {
             // Feldnamen einfügen
             fields.push(headElm.dataset.col);
-        }) // forEach Head Element
+        }); // forEach Head Element
+
+        // InfoTabelle erstellen
+        const infoTable = new InfoTable(tableElm, iList, fields);
+        tableElm.infoTable = infoTable;
 
         // Infolist Daten holen
         iList.fetch().then(() => {
@@ -498,9 +597,12 @@ export function checkTables() {
                 sortIndex = iList.getSortIndex(sortString.split(","));
             }
 
-            // HTML Daten von Liste erzeugen und anzeigen
-            const innerHTML = iList.getTableDataHTML(fields, sortIndex);
-            tbodyElm.insertAdjacentHTML("beforeend", innerHTML);
-        })
-    }) // forEach Table Element
+            infoTable.setSortIndex(sortIndex);
+
+            // HTML Daten von Tabelle erzeugen und anzeigen
+            infoTable.showData();
+            // const innerHTML = iList.getTableDataHTML(fields, sortIndex);
+            // tbodyElm.insertAdjacentHTML("beforeend", innerHTML);
+        });
+    }); // forEach Table Element
 } // checkTables
